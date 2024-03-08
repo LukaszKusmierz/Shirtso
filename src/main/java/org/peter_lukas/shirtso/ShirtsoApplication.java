67f2d52b9c69f87b8da9f6462bc8df1c1f8bb336
LukaszKusmierz <luckilucek303@gmail.com>
2024-03-08 13:51:42 +0100

    bootstrap project

---
+++ src/main/java/org/peter_lukas/shirtso/ShirtsoApplication.java
@@ -0,0 +1,13 @@
+package org.peter_lukas.shirtso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirtsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirtsoApplication.class, args);
+    }
+
+}
