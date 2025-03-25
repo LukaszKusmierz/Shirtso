e0cba8a417095fda7a3b305b876a456a25ca5961
Lukasz Kusmierz <lukasz@lukspeed.eu>
2025-03-25 16:08:14 +0100

    displaying images next updates

--- src/main/java/org/peter_lukas/shirtso/commercial/product/image/ProductImageMappingRepository.java
+++ src/main/java/org/peter_lukas/shirtso/commercial/product/image/ProductImageMappingRepository.java
@@ -1,6 +1,7 @@
 package org.peter_lukas.shirtso.commercial.product.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.UUID;
 
 public interface ProductImageMappingRepository extends JpaRepository<ProductImageMapping, ProductImageMappingId> {
 
+    @Query("SELECT pim FROM ProductImageMapping pim JOIN FETCH pim.image i WHERE " +
+            "pim.product.productId = :productId ORDER BY pim.displayOrder ASC")
     List<ProductImageMapping> findByProduct_ProductIdOrderByDisplayOrderAsc(UUID productId);
 
     Optional<ProductImageMapping> findByProduct_ProductIdAndIsPrimaryTrue(UUID productId);
--- src/main/java/org/peter_lukas/shirtso/config/WebConfig.java
+++ src/main/java/org/peter_lukas/shirtso/config/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", configuration);
+        source.registerCorsConfiguration("/resources/static/**", configuration);
         return source;
     }
 
