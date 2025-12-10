047e5cb7344369658c8bf2f1355c41731a82b412
LukaszKusmierz <lukasz@lukspeed.eu>
2025-12-10 12:21:44 +0100

    n+1 problem in payment repo solved

--- src/main/java/org/peter_lukas/shirtso/commercial/payment/PaymentRepository.java
+++ src/main/java/org/peter_lukas/shirtso/commercial/payment/PaymentRepository.java
@@ -1,6 +1,5 @@
 package org.peter_lukas.shirtso.commercial.payment;
 
-import org.peter_lukas.shirtso.commercial.cart.dto.CartDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +9,6 @@ import java.util.Optional;
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
     Optional<Payment> findByOrderOrderId(Integer orderId);
+
+    Optional<Payment> findByTransactionId(String transactionId);
 }
